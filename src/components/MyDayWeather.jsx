459e020f230b7bd77585c70151557cb17bfad137
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-04-11 13:11:08 +0200

    fix vari

--- src/components/MyDayWeather.jsx
+++ src/components/MyDayWeather.jsx
@@ -1,5 +1,11 @@
 import { Card, Row, Col } from "react-bootstrap";
 
+const getHour  = (dateMs) =>{
+    const dateObject = new Date(dateMs); 
+    console.log(dateObject);
+    const hours = dateObject.getHours(); 
+    return hours;
+}
 
 const MyDayWeather = (props) => {
     const dayWeathers = [...props.weatherObj.list].slice(1, 5);
@@ -12,8 +18,9 @@ const MyDayWeather = (props) => {
                     <Col sm={3} key={index}>
                         <Card style={{ backgroundColor: "grey" }}>
                             <Card.Body>
-                                <Card.Title>City</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">°C</Card.Subtitle>
+                                <Card.Title>{getHour(dayWeather.dt)}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{dayWeather.main.temp} °C</Card.Subtitle>
+                                <img src={`http://openweathermap.org/img/wn/${dayWeather.weather[0].icon}.png`} alt="Weather Icon" />
                             </Card.Body>
                         </Card>
                     </Col>
